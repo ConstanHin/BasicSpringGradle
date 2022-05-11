a107e54a9a681ec20361aa9a979f4272aca9f06d
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-11 12:32:09 +0200

    init basic spring gradle project

---
+++ src/main/java/paquete/BasicProjectSpringGradleApplication.java
@@ -0,0 +1,13 @@
+package paquete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectSpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProjectSpringGradleApplication.class, args);
+	}
+
+}
